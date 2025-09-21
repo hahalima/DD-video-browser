ccde5204b595140572635fb2e945f23f8dc38fb4
hahalima <9022415+hahalima@users.noreply.github.com>
2025-09-21 14:06:41 -0400

    Add testing setup with jest and RTL

---
+++ client/jest.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest"
+  },
+  moduleNameMapper: {
+    // stub plain CSS imports so tests don't crash on `import "./x.css"`
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy"
+  },
+  testPathIgnorePatterns: ["/node_modules/"]
+}
+
+module.exports = config;
